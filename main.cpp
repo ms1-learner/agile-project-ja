5969ad97b3587ad2a5429ea1a0cdea73b997037e
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-02-20 23:23:33 +0900

    Add continuous calculation mode

--- main.cpp
+++ main.cpp
@@ -2,35 +2,76 @@
 
 #include "math_operations.h"
 
-int main() {
+float execute_calculation(float num1, char symbol, float num2)
+{
+  float out{0};
+  try
+  {
+    if (symbol == '+')
+    {
+      out = MathOps::addition(num1, num2);
+    }
+    else if (symbol == '-')
+    {
+      out = MathOps::subtraction(num1, num2);
+    }
+    else if (symbol == '*')
+    {
+      out = MathOps::multiplication(num1, num2);
+    }
+    else if (symbol == '/')
+    {
+      out = MathOps::division(num1, num2);
+    }
+    else if (symbol == 'r')
+    {
+      out = MathOps::sqrtroot(num1);
+    }
+    else if (symbol == '^')
+    {
+      out = MathOps::exponentiation(num1, num2);
+    }
+    else
+    {
+      std::cout << "Input symbol is invalid. Exit calculation." << std::endl;
+      exit(1);
+    }
+    std::cout << "Current Result: " << out << std::endl;
+  }
+  catch (const std::runtime_error &e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+    exit(1);
+  }
+  return out;
+}
+
+int main()
+{
   float num1{0}, num2{0};
   char symbol{'a'};
-  float out{0};
+  float current_result{0};
 
   std::cout << "Enter a calculation: ";
   std::cin >> num1 >> symbol >> num2;
+  current_result = execute_calculation(num1, symbol, num2);
 
-  try {
-    if (symbol == '+') {
-      out = MathOps::addition(num1, num2);
-    } else if (symbol == '-') {
-      out = MathOps::subtraction(num1, num2);
-    } else if (symbol == '*') {
-      out = MathOps::multiplication(num1, num2);
-    } else if (symbol == '/') {
-      out = MathOps::division(num1, num2);
-    } else if (symbol == 'r') {
-      out = MathOps::sqrtroot(num1);
-    } else if (symbol == '^'){
-      out = MathOps::exponentiation(num1, num2);
-    } else {
-      std::cout << "Input is invalid." << std::endl;
+  while (1)
+  {
+    std::cout << "If you want to continue calculation for the current result '" << current_result
+              << "', press 'c', if you want to exit, press 'e'." << std::endl;
+    char next_action{'a'};
+    std::cin >> next_action;
+    if (next_action == 'c')
+    {
+      std::cout << "Please input the calculation symbol and number." << std::endl;
+      std::cin >> symbol >> num2;
+      current_result = execute_calculation(current_result, symbol, num2);
     }
-    
-    std::cout << "Result: " << out << std::endl;
-    
-  } catch (const std::runtime_error& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
+    else if (next_action == 'e')
+    {
+      break;
+    }
   }
 
   return 0;
